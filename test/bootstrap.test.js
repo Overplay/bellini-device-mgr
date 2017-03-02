1d7e4bf944bb0b4a0fbd2625f801bc2fb7fa146f
erikphillips <erik.phillips@icloud.com>
2017-03-01 18:37:31 -0800

    added chai mocha and dummy test

--- test/bootstrap.test.js
+++ test/bootstrap.test.js
@@ -2,23 +2,23 @@
  * Created by erikphillips on 3/1/17.
  */
 
-var sails = require('sails');
-
-before(function(done) {
-
-    // Increase the Mocha timeout so that Sails has enough time to lift.
-    this.timeout(5000);
-
-    sails.lift({
-        // configuration for testing purposes
-    }, function(err) {
-        if (err) return done(err);
-        // here you can load fixtures, etc.
-        done(err, sails);
-    });
-});
-
-after(function(done) {
-    // here you can clear fixtures, etc.
-    sails.lower(done);
-});
+// var sails = require('sails');
+//
+// before(function(done) {
+//
+//     // Increase the Mocha timeout so that Sails has enough time to lift.
+//     this.timeout(5000);
+//
+//     sails.lift({
+//         // configuration for testing purposes
+//     }, function(err) {
+//         if (err) return done(err);
+//         // here you can load fixtures, etc.
+//         done(err, sails);
+//     });
+// });
+//
+// after(function(done) {
+//     // here you can clear fixtures, etc.
+//     sails.lower(done);
+// });
