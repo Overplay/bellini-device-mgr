2a9668dbd8553dbdfd1118a21553e46d7cbfed09
erikphillips <erik.phillips@icloud.com>
2017-03-03 16:46:02 -0800

    AppDataController normal flow tests complete and working

--- test/integration/controllers/AppDataController.test.js
+++ test/integration/controllers/AppDataController.test.js
@@ -43,27 +43,119 @@ describe("AppDataController Tests", function () {
     //        });
     // });
 
-    it("simple post request using appid=emp deviceid=1", function (done) {
-        var req = request.post("/appmodel/emp/2");
-        req.send({
-            "appId":"erikdev",
-            "deviceId":"1234",
-            "data": {"red": 10}
+    // it("simple post request using appid=emp deviceid=1", function (done) {
+    //     var req = request.post("/appmodel/emp/2");
+    //     req.send({
+    //         "appId":"erikdev",
+    //         "deviceId":"1234",
+    //         "data": {"red": 10}
+    //     });
+    //     req.end(function (err, res) {
+    //         if (err) {
+    //             throw err
+    //         }
+    //
+    //         assert.equal(res.status, 200, "expecting a 200 status");
+    //         assert.property(res.body, "forAppId", "checking for appid field");
+    //         assert.property(res.body, "forAppId", "erik", "checking appid");
+    //
+    //         done();
+    //     })
+    // });
+
+    describe("Normal AppData Flow", function () {
+        it("POST request to add new entry into database", function ( done ) {
+            var req = request.post("/appmodel/test/1");
+            req.send({
+                "appId": "test",
+                "deviceId": "1",
+                "data": {"red": 10}
+            });
+            req.end(function (err, res) {
+
+                assert.isNull(err, "null error check");
+                assert.equal(res.status, 200, "check for 200 status");
+
+                assert.isObject(res.body, "check for returned body");
+                assert.property(res.body, "forAppId", "check for appid property");
+                assert.property(res.body, "forDeviceId", "check for deviceid property");
+                assert.property(res.body, "forAppId", "test", "check appid value");
+                assert.property(res.body, "forDeviceId", 1, "check for deviceid value");
+
+                done();
+            });
         });
-        req.end(function (err, res) {
-            if (err) {
-                throw err
-            }
 
-            assert.equal(res.status, 200, "expecting a 200 status");
-            assert.property(res.body, "forAppId", "checking for appid field");
-            assert.property(res.body, "forAppId", "erik", "checking appid");
+        it("PUT request to change the entry in the database", function ( done ) {
+            var req = request.put("/appmodel/test/1");
+            req.send({
+                "appId": "test",
+                "deviceId": "1",
+                "data": {"red": 20}
+            });
+            req.end(function (err, res) {
+                assert.isNull(err, "null error check");
+                assert.equal(res.status, 200, "check for 200 status");
 
-            done();
-        })
+                assert.isObject(res.body[0], "check for returned body");
+                assert.property(res.body[0], "forAppId", "check for appid property");
+                assert.property(res.body[0], "forDeviceId", "check for deviceid property");
+                assert.property(res.body[0], "forAppId", "test", "check appid value");
+                assert.property(res.body[0], "forDeviceId", 1, "check for deviceid value");
+
+                assert.deepProperty(res.body[0], "data.red", "check for data.red");
+                assert.deepPropertyVal(res.body[0], "data.red", 20, "check data.red value");
+
+                done();
+            });
+        });
+
+        it("DELETE request to remove the entry from the database", function ( done ) {
+            var req = request.delete("/appmodel/test/1");
+            req.end(function (err, res) {
+                assert.isNull(err, "null error check");
+                assert.equal(res.status, 200, "check for 200 status");
+
+                assert.isObject(res.body[0], "check for returned body");
+                assert.property(res.body[0], "forAppId", "check for appid property");
+                assert.property(res.body[0], "forDeviceId", "check for deviceid property");
+                assert.property(res.body[0], "forAppId", "test", "check appid value");
+                assert.property(res.body[0], "forDeviceId", 1, "check for deviceid value");
+
+                done();
+            });
+        });
     });
 
-    it("GET request");
-    it("PUT request");
-    it("POST request");
+    describe("GET Error Checking", function () {
+        it("GET request to verify no entry in database", function ( done ) {
+            done();
+        });
+    });
+
+    describe("POST Error Checking", function () {
+        it("POST request to add entry to database", function ( done ) {
+            done();
+        });
+
+        it("POST request to add duplicate entry to database", function ( done ) {
+            done();
+        });
+
+        it("DELETE request to clean up after posts", function ( done ) {
+            done();
+        });
+    });
+
+    describe("PUT Error Checking", function () {
+        it("PUT request to modify non-existent entry in database", function ( done ) {
+            done();
+        });
+    });
+
+    describe("DELETE Error Checking", function () {
+        it("DELETE request to remove non-existent entry in database", function ( done ) {
+            done();
+        });
+    });
 });
