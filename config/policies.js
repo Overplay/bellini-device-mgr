101de4fc23051100dea157742fe63bdf83ff3630
Mitch Kahn <mitch@ourglass.io>
2017-05-28 23:22:16 -0700

    JWT passtru for iOS

--- config/policies.js
+++ config/policies.js
@@ -95,7 +95,7 @@ module.exports.policies = {
 
     // TODO: this is very insecure, for now
     OGDevice: {
-        '*': 'sessionAuth',
+        '*': true,
         'purge': ['isRingAdmin', 'isDELETE'],
         'regcode': ['isDevice'],
         // 'findByRegCode': true,
