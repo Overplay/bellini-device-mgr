f6a033e4216262a361e1f2774009a9f48dbdac13
Logan Saso <logan.saso2016@gmail.com>
2017-07-13 15:54:31 -0700

    Sizes properly on start, but not on change

--- assets/blueline/sim/app/components/mainframe.component.js
+++ assets/blueline/sim/app/components/mainframe.component.js
@@ -15,10 +15,10 @@ app.component( 'mainFrame', {
         widget:  '<',
         vid:     '<'
     },
-    controller: function ( toastr, $log ) {
+    controller: function ( $timeout, $http, toastr, $log ) {
 
         var ctrl = this;
-
+        console.log(ctrl);
 
         this.crawlerPos = function () {
             return (ctrl.crawler.pos !== 0) ? 'cpos-top' : 'cpos-bot';
@@ -29,19 +29,20 @@ app.component( 'mainFrame', {
         }
 
         this.$onInit = function () {
-            $log.debug( "on init()" );
+            $log.debug("on init()");
         }
 
-
+            
+// ng-init="$ctrl.crawlerHeight = $ctrl.getHeight($ctrl.crawler.src)"  FAILURE LINES
+// ng-init="$ctrl.widgetHeight = $ctrl.getHeight($ctrl.widget.src)"    FAILURE LINES
     },
     template:   `
         <div class="tvframe">
             <video class="simvid" autoplay loop>
                 <source src="{{ $ctrl.vid }}" type="video/mp4">
             </video>
-            <iframe class="crawler" ng-class="$ctrl.crawlerPos()" src="{{ $ctrl.crawler.src }}" scrolling="no"></iframe>
-            <iframe class="widget" ng-class="$ctrl.widgetPos()" src="{{ $ctrl.widget.src }}" scrolling="no"></iframe>
-
+            <iframe class="crawler" ng-style="{'height': $ctrl.crawler.height  + '%'}" ng-class="$ctrl.crawlerPos()" src="{{ $ctrl.crawler.src }}" scrolling="no"></iframe>
+            <iframe class="widget"  ng-style="{'height': $ctrl.widget.height + '%'}"  ng-class="$ctrl.widgetPos()" src="{{ $ctrl.widget.src }}" scrolling="no"></iframe>
         </div>
     
     `
