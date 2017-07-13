5390cc55da6a6647903546912c1c5a3cd1a05c1f
Logan Saso <logan.saso2016@gmail.com>
2017-07-13 10:21:53 -0700

    Revert "Doing gulp stuff"
    This reverts commit f45e5a27a72196aac8356be2fb012383829f7def.

--- assets/js/uiOG/gulpfile.js
+++ assets/js/uiOG/gulpfile.js
@@ -6,25 +6,8 @@ var templateCache = require('gulp-angular-templatecache');
 var replace = require('gulp-replace');
 var rename = require('gulp-rename');
 
-gulp.task('uibcache', function () {
-    return gulp.src('*.html')
-        .pipe(templateCache('templates.js', {
-            templateHeader: '//Start of templates\n',
-            templateFooter: '\n//End of templates\n'
-
-        }))
-        .pipe(gulp.dest('.'));
-
-});
-
-gulp.task('build', ['uibcache'], function () {
-
-    var fs = require('fs');
-    var templates = fs.readFileSync('./templates.js', "utf8");
-    //console.log(templates);
-    return gulp.src('uiOG.notemplates.js')
-        .pipe(replace('//<templates>', templates))
-        .pipe(rename('uiOG.tpls.js'))
-        .pipe(gulp.dest('.'));
-
-});
+gulp.task( 'uibcache', function () {
+    return gulp.src( '*.html' )
+        .pipe( templateCache() )
+        .pipe( gulp.dest( '.' ) );
+} );
