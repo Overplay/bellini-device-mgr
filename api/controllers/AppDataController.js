9684d7fffa970f797f18b2e9a3d70968c4727257
Mitch Kahn <mitch@ourglass.io>
2017-07-29 18:36:42 -0700

    ogAPIBL2 is in the house! Partial rewrite to connect to venue and device data. It is NOT fully compatible with ogAPIBL.

--- api/controllers/AppDataController.js
+++ api/controllers/AppDataController.js
@@ -5,6 +5,8 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 
+var Promise = require( 'bluebird' );
+
 function hasUDIDandAppId( req ) {
 
     var params = req.allParams();
@@ -12,6 +14,40 @@ function hasUDIDandAppId( req ) {
         return undefined;
 
     return params;
+
+}
+
+function retrieveAppDataAndDevice( dataScope, appid, deviceid ) {
+
+    var devicePromise = OGDevice.findOne( { deviceUDID: deviceid } );
+
+    if ( dataScope === 'device' ) {
+
+        return Promise.props( {
+            data:   AppData.findOne( { forAppId: appid, forDeviceUDID: deviceid } ),
+            device: devicePromise
+        } );
+
+    } else {
+        // scope is venue
+        return devicePromise
+            .then( function ( device ) {
+                if ( !device )
+                    throw new Error( "No such device" );
+
+                return Promise.props( {
+                    data:   AppData.findOne( {
+                        forAppId:      appid,
+                        forVenueId:    device.atVenueUUID,
+                        forDeviceUDID: 'venue'
+                    } ),
+                    device: devicePromise
+                } );
+
+            } )
+
+    }
+
 
 }
 
@@ -35,6 +71,118 @@ module.exports = {
 
     },
 
+    appDataForDevice: function ( req, res ) {
+
+        var params = req.allParams();
+        var appid = params.appid;
+        var deviceid = params.deviceid;
+        var dataScope = params.scope || 'device';
+
+        switch ( req.method ) {
+
+            case 'POST':
+
+                sails.log.silly( "POSTING app data for: " + appid + ' on device ' + deviceid + ' with scope: ' + dataScope );
+
+
+                //AppData.findOne( { forAppId: appid, forDeviceUDID: deviceid } )
+                retrieveAppDataAndDevice( dataScope, appid, deviceid )
+                    .then( function ( props ) {
+                        if ( props.data ) {
+                            return res.badRequest( { error: "model already exists, try a PUT genius!" } );
+                        }
+
+                        var newAppData = (dataScope === 'device') ?
+                            {
+                                forAppId:      appid,
+                                forDeviceUDID: deviceid,
+                                data:          params.data || {}
+                            } :
+                            {
+                                forAppId:      appid,
+                                data:          params.data || {},
+                                forVenueId:    props.device.atVenueUUID,
+                                forDeviceUDID: 'venue'
+                            };
+
+                        AppData.create( newAppData )
+                            .then( function ( model ) {
+                                if ( !model ) {
+                                    return res.serverError( { error: "could not make model" } );
+                                }
+                                return res.ok( model );
+                            } )
+                            .catch( res.serverError )
+
+                    } )
+                    .catch( res.serverError );
+                break;
+
+            case 'PUT':
+                sails.log.silly( "PUTTING app data for: " + appid + " on device " + deviceid );
+
+                var newAppData = { data: params.data || {} };
+
+                //AppData.findOne( { forAppId: appid, forDeviceUDID: deviceid } )
+                retrieveAppDataAndDevice( dataScope, appid, deviceid )
+                    .then( function ( props ) {
+                        if ( !props.data ) {
+                            return res.badRequest( { error: "model does not exist, try a POST" } );
+                        }
+
+                        AppData.update( { id: props.data.id }, newAppData )
+                            .then( function ( model ) {
+                                if ( !model || !model.length ) {
+                                    return res.serverError( { error: "unable to update model" } );
+                                }
+                                // TODO could this ever not be an array?
+                                var m = model[ 0 ];
+                                AppData.publishUpdate( m.id, m.data, req );
+                                return res.ok( m.data );
+                            } )
+                            .catch( res.serverError );
+                    } )
+                    .catch( res.serverError );
+
+                break;
+
+            case 'GET':
+                sails.log.silly( "GETTING app data for: " + appid + " on device " + deviceid );
+
+                //AppData.findOne( { forAppId: appid, forDeviceUDID: deviceid } )
+                retrieveAppDataAndDevice( dataScope, appid, deviceid )
+                    .then( function ( props ) {
+                        if ( !props.data ) {
+                            return res.badRequest( { error: "model does not exist!" } );
+                        }
+                        return res.ok( props.data )
+                    } )
+                    .catch( res.serverError );
+
+                break;
+
+            case 'DELETE':
+                sails.log.silly( "DELETE app data for: " + appid + " on device " + deviceid );
+
+                // TODO: These feels vaguely dirty
+                retrieveAppDataAndDevice( dataScope, appid, deviceid )
+                    .then( function ( props ) {
+                        if ( !props.data ) {
+                            return res.badRequest( { error: "model does not exist" } );
+                        }
+                        return AppData.destroy( { id: props.data.id } )
+                            .then( res.ok )
+                            .catch( res.serverError )
+                    } );
+
+                break;
+
+            default:
+                return res.ok( "Not implemented" );
+        }
+
+    },
+
     // TAKES in appid, deviceid, venueid
     // GET appdata/:appid/:deviceid
     // GET appdata
@@ -42,27 +190,40 @@ module.exports = {
     // get /appmodel/:appid/:deviceid
     // http://localhost:2001/appmodel/erik/12345
 
-    appDataForDevice: function ( req, res ) {
+    appDataForDeviceOld: function ( req, res ) {
 
         var params = req.allParams();
         var appid = params.appid;
         var deviceid = params.deviceid;
+        var dataScope = params.scope || 'device';
 
         switch ( req.method ) {
 
             case 'POST':
-                sails.log.silly( "POSTING app data for: " + appid + ' on device ' + deviceid );
-                AppData.findOne( { forAppId: appid, forDeviceUDID: deviceid } )
+
+                sails.log.silly( "POSTING app data for: " + appid + ' on device ' + deviceid + ' with scope: ' + dataScope );
+
+
+                //AppData.findOne( { forAppId: appid, forDeviceUDID: deviceid } )
+                retrieveAppData( dataScope, appid, deviceid )
                     .then( function ( model ) {
                         if ( model ) {
                             return res.badRequest( { error: "model already exists, try a PUT genius!" } );
                         }
 
-                        var newAppData = {
-                            forAppId: appid,
-                            forDeviceUDID: deviceid,
-                            data: params.data || {}
-                        };
+                        var newAppData = (dataScope === 'device') ?
+                            {
+                                forAppId:      appid,
+                                forDeviceUDID: deviceid,
+                                data:          params.data || {}
+                            } :
+                            {
+                                forAppId: appid,
+                                data:     params.data || {}
+
+
+                            };
+
                         AppData.create( newAppData )
                             .then( function ( model ) {
                                 if ( !model ) {
@@ -171,6 +332,46 @@ module.exports = {
 
     },
 
+    // Returns appdata for app for device, or creates and entry from the prototype in the App entry
+    // TODO: This is a lot of code duplication
+    initializeVenueData: function ( req, res ) {
+
+        if ( req.method != 'POST' )
+            return res.badRequest( { error: "bad verb" } );
+
+        var params = hasUDIDandAppId( req );
+        if ( !params )
+            return res.badRequest( { error: "missing udid or appid" } );
+
+        //AppData.findOne( { forDeviceUDID: params.deviceUDID, forAppId: params.appid } )
+        retrieveAppDataAndDevice( 'venue', params.appid, params.deviceUDID )
+            .then( function ( props ) {
+                if ( props.data )
+                    return res.ok( props.data );
+
+                App.findOne( { appId: params.appid } )
+                    .then( function ( app ) {
+                        if ( !app )
+                            return res.badRequest( { error: "no such app" } );
+
+
+                        var vappD = {
+                            forAppId:      params.appid,
+                            data:          app.defaultModel || {},
+                            forVenueId:    props.device.atVenueUUID,
+                            forDeviceUDID: 'venue'
+                        };
+
+                        AppData.create( vappD )
+                            .then( res.ok )
+                            .catch( res.serverError );
+                    } )
+                    .catch( res.serverError );
+            } )
+            .catch( res.serverError );
+
+    },
+
     // WEBSOCKETS subscription method. Must be called over sockets
     subscribe: function ( req, res ) {
 
@@ -178,7 +379,7 @@ module.exports = {
             return res.badRequest( { error: "Sockets only, sonny" } );
         }
 
-        if ( req.method != 'POST' )
+        if ( req.method !== 'POST' )
             return res.badRequest( { error: "That's not how to subscribe, sparky!" } );
 
         //OK, we need a deviceUDID
@@ -187,15 +388,32 @@ module.exports = {
         if ( !params.appid || !params.deviceUDID )
             return res.badRequest( { error: "Missing params" } );
 
-        AppData.findOne( { forDeviceUDID: params.deviceUDID, forAppId: params.appid } )
-            .then( function ( model ) {
+        OGDevice.findOne( { deviceUDID: params.deviceUDID })
+            .then( function(device){
 
-                if ( !model )
-                    return res.badRequest( { error: "No such model" } );
+                if (!device){
+                    return res.badRequest({ error: "no such device" });
+                }
 
-                AppData.subscribe( req, model.id );
-                return res.ok( model )
+                // ok, at this point we need to grab the ids for the A+D and A+V entries
+                var promises = [
+                    // A+D
+                    AppData.findOne( {
+                        forDeviceUDID: params.deviceUDID,
+                        forAppId: params.appid } ),
+                    // A+V
+                    AppData.findOne( {
+                        forAppId:      params.appid,
+                        forVenueId:    device.atVenueUUID,
+                        forDeviceUDID: 'venue'
+                    } )];
 
+                return Promise.all(promises);
+
+            })
+            .then( function ( models ) {
+                AppData.subscribe( req, models );
+                return res.ok( models )
             } )
             .catch( res.serverError );
 
--- assets/blueline/opp/io.ourglass.nowserving/app/tv/app/components/mainscreen/mainscreen.controller.js
+++ assets/blueline/opp/io.ourglass.nowserving/app/tv/app/components/mainscreen/mainscreen.controller.js
@@ -9,8 +9,16 @@ app.controller( "mainScreenController", function ( $scope, $log, ogAPI ) {
     $scope.ticketNumber = 0;
 
     function modelChanged( newValue ) {
-
+        $scope.sourceVenue = false;
         $log.info( "Model changed, yay!" );
+        $scope.ticketNumber = newValue.ticketNumber;
+        // $scope.$apply();
+    }
+
+    function venueModelChanged( newValue ) {
+
+        $log.info( "Venue model changed, yay!" );
+        $scope.sourceVenue = true;
         $scope.ticketNumber = newValue.ticketNumber;
         // $scope.$apply();
     }
@@ -24,14 +32,18 @@ app.controller( "mainScreenController", function ( $scope, $log, ogAPI ) {
 
         ogAPI.init( {
             appName:      "io.ourglass.nowserving",
-            sockets:      true,
-            modelCallback: modelChanged,
+            deviceModelCallback: modelChanged,
+            venueModelCallback: venueModelChanged,
             messageCallback: inboundMessage,
             appType:      'tv'
         })
             .then ( function ( data ) {
-                $log.debug("ogAPI init complete!");
-                $scope.ticketNumber = data.ticketNumber;
+                $log.debug( "ogAPI init complete!" );
+                if (data.venue && data.device.useVenueData){
+                    $scope.ticketNumber = data.venue.ticketNumber || '??';
+                } else {
+                    $scope.ticketNumber = data.device.ticketNumber;
+                }
             })
             .catch( function ( err ) {
                 $log.error("Something failed: " + err);
