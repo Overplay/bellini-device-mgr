91ae8e62f8eac4743bcf6b4d3017114e1a8155b1
Logan Saso <logan.saso2016@gmail.com>
2017-08-14 11:53:39 -0700

    Did a little work on judging phase

--- assets/blueline/opp/io.ourglass.cardsagainsthumanity/app/control/app/components/judging/judging.controller.js
+++ assets/blueline/opp/io.ourglass.cardsagainsthumanity/app/control/app/components/judging/judging.controller.js
@@ -1,5 +1,19 @@
 app.controller('judgingController', function ($scope, cah) {
 
+	var roundPlayingCards = cah.roundPlayingCards;
+	$scope.player = cah.player;
 
+	$scope.findCard = function findCard() {
+		return _.find(roundPlayingCards, function (card) {
+			return player.id == card.submittedBy.id;
+		});
+	};
+
+	$scope.amJudge = function amJudge() {
+		return $scope.player.id == cah.judgeIndex % cah.players.length;
+	};
+	$scope.findJudge = function findJudge() {
+		return cah.getPlayerById(cah.judgeIndex % cah.players.length).name;
+	};
 
 });
