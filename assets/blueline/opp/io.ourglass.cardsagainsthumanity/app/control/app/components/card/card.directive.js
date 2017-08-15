94eba773fcc550318ccacd4d9adc91d692399cd9
Logan Saso <logan.saso2016@gmail.com>
2017-08-15 15:13:18 -0700

    Changed ID Display to debug mode. Added Ourglass display normally.

--- assets/blueline/opp/io.ourglass.cardsagainsthumanity/app/control/app/components/card/card.directive.js
+++ assets/blueline/opp/io.ourglass.cardsagainsthumanity/app/control/app/components/card/card.directive.js
@@ -11,7 +11,7 @@ app.directive('card', function (cah, $log) {
 		},
 		templateUrl: 'app/components/card/card.partial.html',
 		link: function (scope, element, attrs) {
-
+			scope.debugMode = false;
 			$log.debug("Card directive entered");
 
 		}
