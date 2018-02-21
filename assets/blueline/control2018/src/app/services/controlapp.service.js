9c989822d495ce8b0422ba8467b31f2b88c24126
Mitch Kahn <mitch@ourglass.io>
2018-02-21 10:38:17 -0800

    Debugging

--- assets/blueline/control2018/src/app/services/controlapp.service.js
+++ assets/blueline/control2018/src/app/services/controlapp.service.js
@@ -175,7 +175,7 @@ export default class ControlAppService {
             console.error("Nulling currentProgramGrid");
             this.currentProgramGrid = null;
         } else {
-            this.currentProgramGrid = _.find( this.currentGrid, { channel: { channelNumber: this.currentProgram.channelNumber } } );
+            this.currentProgramGrid = _.find( this.currentGrid, { channel: { channelNumber: haveCurrentChannel } } );
             console.log( "Set currentProgramGrid to: " + this.currentProgramGrid );
         }
 
