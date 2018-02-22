b3b4d07ac974f05162a8392a5fff163c069d2d0c
Mitch Kahn <mitch@ourglass.io>
2018-02-21 17:27:26 -0800

    Nearly done control2!

--- assets/blueline/control2018/src/app/components/dashboard/dash.component.js
+++ assets/blueline/control2018/src/app/components/dashboard/dash.component.js
@@ -81,20 +81,23 @@ const Component = {
                         ng-click="$ctrl.meplay()"><span class="glyphicon glyphicon-headphones"></span>&nbsp;LISTEN
                 </button>
             </div>
+        </div>
+
+    </div>
+    <div class="row" >
+        <div class="col-sm-12">
             <h4 class="text-og-green">RUNNING APPS</h4>
             <p class="no-apps" ng-show="$ctrl.apps.running.length < 1">There are no running apps.</p>
             <app-cell ng-repeat="app in $ctrl.apps.running | orderBy: 'displayName' track by $index" 
                       app="app" running="true"
                       permissions="$ctrl.permissions"></app-cell>
-        </div>
-
-    </div>
-    <div class="row" ng-if="$ctrl.permissions.anymanager && !$ctrl.cntrlSvc.isMasqueradingAsPatron">
-        <div class="col-sm-12">
-            <h4 class="text-og-orange">AVAILABLE APPS Bitches!</h4>
-            <p class="no-apps" ng-show="$ctrl.apps.available.length < 1">There are no available apps.</p>
-            <app-cell ng-repeat="app in $ctrl.apps.available | orderBy: 'displayName' track by $index"
-                app="app" permissions="$ctrl.permissions"></app-cell>
+            <div ng-if="$ctrl.permissions.anymanager && !$ctrl.cntrlSvc.isMasqueradingAsPatron">
+                <h4 class="text-og-orange">AVAILABLE APPS</h4>
+                <p class="no-apps" ng-show="$ctrl.apps.available.length < 1">There are no available apps.</p>
+                <app-cell ng-repeat="app in $ctrl.apps.available | orderBy: 'displayName' track by $index"
+                    app="app" permissions="$ctrl.permissions"></app-cell>
+            </div>
+            
         </div>
     </div>
 </div>
